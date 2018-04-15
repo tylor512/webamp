bf99a49848122435deebcccd695b03b68d7c5690
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-04-15 18:13:54 +0000

    Deploy captbaritone/webamp to github.com/captbaritone/webamp.git:gh-pages

---
+++ precache-manifest.ec811169b5431bcbe89950eb1ae491d7.js
@@ -0,0 +1,69 @@
+self.__precacheManifest = [
+  {
+    "url": "/skins/MacOSXAqua1-5-88dbd4e043795c98625462a908a2d965.wsz"
+  },
+  {
+    "url": "/jsmediatags-f8e4c3ed89b962809283.js"
+  },
+  {
+    "url": "/mp3/llama-2.91-deed9f60e1c2a04e753d90c72d57bea1.mp3"
+  },
+  {
+    "url": "/skins/Green-Dimension-V2-4308a2fc648033bf5fe7c4d56a5c8823.wsz"
+  },
+  {
+    "url": "/skins/ZaxonRemake1-0-c7333f1f8fd296f4dd68f9b138d22e5d.wsz"
+  },
+  {
+    "url": "/skins/XMMS-Turquoise-46ab165b574d97e2e51b22cb47a7661c.wsz"
+  },
+  {
+    "url": "/skins/Vizor1-01-0985438a88963f6803311ed99b17f7f4.wsz"
+  },
+  {
+    "url": "/skins/TopazAmp1-2-11f6cf0624da77b1bbe1d48aaef470c2.wsz"
+  },
+  {
+    "url": "/webamp-f8e4c3ed89b962809283.js"
+  },
+  {
+    "url": "/skins/base-2.91-png-ef40f3892df5f6f9bea9634e6c42e4b3.wsz"
+  },
+  {
+    "url": "/images/preview-42304ea56f49c60e3d2c6c97d1bdddeb.png"
+  },
+  {
+    "url": "/images/manifest/icon_192x192.ec9b715176586bf58ec5abe0354efd85.png"
+  },
+  {
+    "url": "/images/manifest/icon_512x512.5228b937afc3368434b594aa65be2c08.png"
+  },
+  {
+    "url": "/images/manifest/icon_513x513.1f27cb4bba614c00bf595696efa4435d.svg"
+  },
+  {
+    "revision": "741d860160f089714672c5dd289af619",
+    "url": "/index.html"
+  },
+  {
+    "url": "/images/manifest/icon-96x96-44fece775401fd6184d71022778b4edd.png"
+  },
+  {
+    "url": "/images/manifest/icon-72x72-9d9aaf3f4dad41359616e32e082dc71c.png"
+  },
+  {
+    "url": "/images/manifest/icon-48x48-67470808290acc080f2d154fd1df70d1.png"
+  },
+  {
+    "url": "/images/manifest/icon-192x192-fd139d3437e0cad44d05f85b226a24d7.png"
+  },
+  {
+    "url": "/images/manifest/icon-144x144-06bb87f22f8080f239f096e856435678.png"
+  },
+  {
+    "url": "/images/favicon-0cc27932e7d5cbda2849b27e6e6dc3c8.ico"
+  },
+  {
+    "url": "/css/page-1f32969bed4f100811ba55f10f284cc8.css"
+  }
+];
